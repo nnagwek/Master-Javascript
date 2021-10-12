f94e8c7db615f6e92c60a6f24069f7926d51c753
Niket Nagwekar <niket.nagwekar@quantiphi.com>
2021-10-12 19:42:53 +0530

    added playground.js for testing purpose

---
+++ Arrays/playground.js
@@ -0,0 +1,8 @@
+const strings = [ 'a', 'b', 'c', 'd'];
+
+console.log(strings[2]);  // O/P  c
+
+//push
+strings.push('e');  // O(1)
+console.log(strings); // O/P  [ 'a', 'b', 'c', 'd', 'e' ]  O(1)
+
